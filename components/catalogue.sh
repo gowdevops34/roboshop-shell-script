3993a25e3da6a339af8f41c939d1fbe834ac445e
gowdevops34 <kgowthami34@gmail.com>
2022-11-26 23:23:28 +0530

    catalogue script

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -3,6 +3,7 @@ yum install nodejs -y
 
 
 useradd roboshop
+
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
 rm -rf catalogue
