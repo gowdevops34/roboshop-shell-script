5b5b548b104c2fa10b1970c506658e7bb9a14128
gowdevops34 <kgowthami34@gmail.com>
2022-11-26 23:15:11 +0530

    catalogue script

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -1,6 +1,7 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 
+
 useradd roboshop
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 cd /home/roboshop
@@ -9,6 +10,7 @@ unzip /tmp/catalogue.zip
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 npm install
+
 
 
 #Update SystemD file with correct IP addresses
