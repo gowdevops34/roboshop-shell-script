bbd1dcee40dd725d0b6324f23b5b19ddf32da893
gowdevops34 <kgowthami34@gmail.com>
2022-12-17 15:12:46 +0530

    user script

--- components/user.sh
+++ components/user.sh
@@ -1,4 +1,4 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash
 yum install nodejs -y
 useradd roboshop
 
@@ -9,12 +9,8 @@ unzip /tmp/user.zip
 mv user-main user
 cd /home/roboshop/user
 npm install
-#1. Update SystemD service file,
 
-#   Update `REDIS_ENDPOINT` with Redis Server IP
-
-#   Update `MONGO_ENDPOINT` with MongoDB Server IP
-
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service
 mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
 systemctl daemon-reload
 systemctl start user
