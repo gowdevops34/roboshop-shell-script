8a9a498e8430cb66ad883626606c72400a3ce87a
gowdevops34 <kgowthami34@gmail.com>
2022-12-22 16:22:09 +0530

    payment script

--- components/cart.sh
+++ components/cart.sh
@@ -1,3 +1,14 @@
+CHECK_ROOT() {
+  USER_ID=$(id -u)
+  if [ $USER_ID -ne 0 ]; then
+    echo You are Non root user
+    echo You can run this script as root user or with sudo
+    exit 1
+  fi
+}
+
+CHECH_ROOT
+
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash
 yum install nodejs -y
 useradd roboshop
