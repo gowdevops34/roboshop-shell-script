5b475ee69f13a0e7c3401fdfe1399e0a327b2e9a
gowdevops34 <kgowthami34@gmail.com>
2022-12-23 09:24:56 +0530

    script

--- components/cart.sh
+++ components/cart.sh
@@ -1,5 +1,30 @@
 source components/common.sh
 
+CHECK_ROOT
+
+PRINT "Setting Up Nodejs YUM Repo is "
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Installing Nodejs"
+yum install nodejs -y &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Creating Application User"
+id roboshop &>>${LOG}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${LOG}
+fi
+CHECK_STAT $?
+
+PRINT "Downloading CArt Content"
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
+
+cd /home/roboshop
+
+PRINT "Remove old Content"
+rm -rf cart &>>${LOG}
 CHECK_STAT $?
 
 PRINT "Extract Cart Content"
